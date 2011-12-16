0a1e80cd33561b7e3ee84b8f8afc578a95eef915
Philippe Langlois <Philippe.Langlois@gmail.com>
2011-12-16 20:45:49 +0100

    full

--- flash.py
+++ flash.py
@@ -6,11 +6,22 @@ import sys
 import pygame
 import random
 
+from pygame.locals import *
+
 pygame.init() 
 
 #create the screen
 
-window = pygame.display.set_mode((1000, 1000)) 
+# window = pygame.display.set_mode((1300, 1000)) 
+
+modes = pygame.display.list_modes(16)
+if not modes:
+     print '16bit not supported'
+else:
+    print 'Found Resolution:', modes[0]
+    pygame.display.set_mode(modes[0], FULLSCREEN, 16)
+# pygame.display.set_mode(modes[0], FULLSCREEN, 16)
+
 
 #pygame.draw.line(window, (255, 0, 255), (0, 0), (30, 50))
 #pygame.draw.line(window, (255, 255, 255), (0, 0), (640, 480))
@@ -51,10 +62,10 @@ ancienposY = 0
 #         print event
 
 while True:
-  couleurR = (couleurR + 1) % 256
-  couleurV = (ancienposX + 2) % 256
-  couleurB = (ancienposY + 3) % 256
-  posSouris = pygame.mouse.get_pos()
+  couleurR = (couleurR + 100) % 256
+  couleurV = (ancienposX + 33) % 256
+  couleurB = (ancienposY + 66) % 256
+  # posSouris = pygame.mouse.get_pos()
   posX = random.randint(0,1000)
   posY = random.randint(0,1000)
   pygame.draw.rect(window, (couleurR, couleurV, couleurB), (ancienposX, ancienposY, posX, posY) )
